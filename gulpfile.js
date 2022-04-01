184cc3ded101f529e75df679921d011f83280587
Neil de Carteret <neil@lumphammer.net>
2022-04-01 17:38:27 +0100

    .

--- gulpfile.js
+++ gulpfile.js
@@ -140,7 +140,7 @@ export async function updateManifestFromCITagPush () {
   if (stripInitialv(tag) !== manifest.version) {
     throw new Error(`Manifest version (${manifest.version}) does not match tag (${tag})`);
   }
-  manifest.download = `${process.env.CI_API_V4_URL}/projects/${encodeURIComponent(path)}/packages/generic/${manifest.name}/latest/${manifestPath}`
+  manifest.download = `${process.env.CI_API_V4_URL}/projects/${encodeURIComponent(path)}/packages/generic/${manifest.name}/${tag}/${manifestPath}`
   console.log({tag, path, manifest});
   fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
 }
